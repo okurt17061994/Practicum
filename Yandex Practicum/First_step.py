0af015de9fce0afcf4bcf906a2b79ea48ac55132
Oybek Kurtyakov <Oybek.Kurtyakov@MECHTA.LOC>
2021-04-08 20:38:16 +0600

    simple_opiratins

--- Yandex Practicum/First_step.py
+++ Yandex Practicum/First_step.py
@@ -1,2 +1,29 @@
-print("Hi world mathafucka!!!")
-print(2 ** 10)
+print(2 + 5 * 4 / 2 - 8 + 3 * (3 + 2)) # - пример операций
+print(2 ** 2) # - возведение в степень
+
+
+# число носителей английского
+english_native = 378.2
+# число говорящих на английском
+english_speakers = 1121
+# число носителей русского
+russian_native = 153.9
+# число говорящих на русском
+russian_speakers = 264.3
+
+# число выучивших язык как второй
+english_second = english_speakers - english_native
+russian_second = russian_speakers - russian_native
+
+print(english_second / russian_second)
+
+
+english = 378.2
+russian = 153.9
+german = 76.0
+chinese = 908.7
+top3_total =(english + russian + german)
+print(chinese - top3_total)
+
+
+
