b0c995610fa9fdc655870bd4ce51535358aa6c73
okurt17061994 <okurtyakov@gmail.com>
2021-08-10 22:23:06 +0400

    100821-correct

---
+++ Test files/if-elif-else.py
@@ -0,0 +1,10 @@
+age = 31
+
+if (age <= 4):
+    print("You are baby")
+elif (age > 4) and (age < 12):
+    print("You are kid")
+elif (age >=12) and (age < 19):
+    print ("You are teen")
+else:
+    print("You are adult")
--- Yandex_Practicum/First_step.py
+++ Yandex_Practicum/First_step.py
@@ -1,97 +1,3 @@
-print(" ")
-# - ПРИМЕРЫ ПРОСТЫХ ВЫЧИСЛЕНИЙ
-# - пример простых вычислительных операций
-
-print("ПРИМЕРЫ ПРОСТЫХ ВЫЧИСЛЕНИЙ")
-print(2 + 5 * 4 / 2 - 8 + 3 * (3 + 2))
-print(" ")
-
-print(" ")
-print("Возведение в степень = 2 ** 2 = ")
-print(2 ** 2) # - возведение в степень
-print(" ")
-print(" ")
-
-
-# - ПЕРЕМЕННЫЕ И ОПЕРАЦИИ С НИМИ
-# число носителей английского
-english_native = 378.2
-# число говорящих на английском
-english_speakers = 1121
-# число носителей русского
-russian_native = 153.9
-# число говорящих на русском
-russian_speakers = 264.3
-
-# число выучивших язык как второй
-english_second = english_speakers - english_native
-russian_second = russian_speakers - russian_native
-
-# конкатенация для вывода результата с описанием
-print("english_second / russian_second = " + str(english_second / russian_second))
-
-# - ИСПОЛЬЗОВАНИЕ ПЕРЕМЕННЫХ
-print("ИСПОЛЬЗОВАНИЕ ПЕРЕМЕННЫХ")
-english = 378.2
-russian = 153.9
-german = 76.0
-chinese = 908.7
-top3_total =(english + russian + german)
-print("top3_total = " + str(chinese - top3_total))
-print(" ")
-print(" ")
-
-# - ТИПЫ ДАННЫХ
-print(type(english))  # - будет выдан результат - <class 'int'>
-# - дробные числа имеют другой тип данных — float
-german_web_percent = 6
-print(type(german_web_percent))
-german_web_percent_2 = 6.0
-print(type(german_web_percent_2))
-print(" ")
-print(" ")
-
-russian_web_part = 0.061
-web_popular = 10000000
-russian_web_popular = russian_web_part * web_popular
-
-print("russian_web_popular и его тип")
-print(russian_web_popular)
-print(type(russian_web_popular))
-print(" ")
-print(" ")
-
-#ПРЕОБРАЗОВАНИЕ ТИПОВ
-russian_native_millions = 153.9
-russian_native = int(russian_native_millions * 1000000)
-print("russian_native = ")
-print(russian_native)
-#Преобразовать тип данных из float в integer - перевод типа данных из float в integer следующим способом
-
-#print(int(russian_native_millions * 1000000))
-
-print(" ")
-print(" ")
-print("Языки в онлайне и оффлайне ")
-total_web = 10
-total_speakers = 7539
-
-chinese_speakers = 1107
-chinese_web_part = 0.017
-print("chinese_web_part = " + str(chinese_web_part))
-
-english_speakers = 1121
-english_web_part = 0.539
-print(english_web_part)
-
-russian_speakers = 264.3
-russian_web_part = 0.061
-print(russian_web_part)
-print(" ")
-print(" ")
-
-#ПЕЧАТЬ НА ОДНОЙ СТРОКЕ
-print("Печать на одной строке")
 total_web = 10
 total_speakers = 7539
 
@@ -209,3 +115,6 @@ print('Доля говорящих на языке: {:.1%}'.format(russian_speak
 print('Доля сайтов с языком: {:.1%}'.format(russian_web_part))
 print('Индекс проникновения в интернет: {:.2f}'.format(russian_index))
 print()
+
+
+
