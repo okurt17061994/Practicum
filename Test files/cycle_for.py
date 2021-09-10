321540e68aff41c272af2cf1192037fede19d50a
okurt17061994 <76731586+okurt17061994@users.noreply.github.com>
2021-09-10 10:40:01 +0400

    add_cycle_for

---
+++ Test files/cycle_for.py
@@ -0,0 +1,14 @@
+emojixpress = [2.26, 19.1, 25.6, 233.0, 15.2, 22.7, 64.6, 87.5, 6.81, 6.0]
+
+# количество всех эмодзи в миллионах
+emojixpress_total = 1720
+
+print('Доли эмодзи:')
+# объявляем цикл
+for element in emojixpress:
+    part = element / emojixpress_total
+
+    print('{:.1%}'.format(part))  # вставьте переменную part вместо многоточия
+
+print()
+print('Всего эмодзи: 1.72 млрд')
