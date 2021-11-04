2b12d096cb82f26a0d2e9f6370b4f532911c84b7
okurt17061994 <76731586+okurt17061994@users.noreply.github.com>
2021-11-04 22:44:00 +0400

    data_structure

--- python_learning/data_typs_structures/1data_typs.py
+++ python_learning/data_typs_structures/1data_typs.py
@@ -1,7 +1,9 @@
 a = 10
 b = 10.5
 c = 'fff'
+t = True
 
 print(type(a))
 print(type(b))
 print(type(c))
+print(type(t))
---
+++ python_learning/data_typs_structures/data_structures.py
@@ -0,0 +1,16 @@
+
+list = [1, 2.2, 'python'] #class 'list' - изменяем
+dog = ["Freddie", 9, True, 1.1, 2001, ["bone", "little ball"]] #вложенный список
+
+tuple = (5,'program', 1+3j) #кортеж - неизменяем
+
+dictionary = {'Огонёк': 'уменьш. от слова "Огонь"', 'Вася': 'уменьш. от слова "Василий"'} #словарь - неизменяем
+
+integer_num_set = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5} # пример множества
+
+print(type(list))
+print(type(dog))
+print(type(tuple))
+print(type(dictionary))
+print(type(integer_num_set))
+
